bb49dda94b03df60d93ac9bcbb9191459e50ec99
Oleg <olyubichenko@gmail.com>
2023-11-07 19:01:19 +0200

    char load

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,39 +1,97 @@
 import { Component } from "react";
 import MarvelService from "../../services/MarvelService";
+import Spinner from "../spinner/spinner";
+import ErrorMessage from "../errorMessage/ErrorMessage";
 
-import './charList.scss';
-import abyss from '../../resources/img/abyss.jpg';
+import "./charList.scss";
 
-class CharList extends Component{
-    state = {
-        chars: {},
-        loading: true,
-        error: false,
-    }
+class CharList extends Component {
+  state = {
+    chars: [],
+    loading: true,
+    error: false,
+    newItemLoading: false,
+    offset: 250,
+    charEnded: false,
+  };
 
-    marvelService = new MarvelService();
+  marvelService = new MarvelService();
 
-    componentDidMount() {
-        this.updateChars();
-      }
-    
-    onCharsLoaded = (chars) => {
-        this.setState({ chars, loading: false });
-        console.log(1);
-    };
+  componentDidMount() {
+    this.onRequest();
+  }
 
-    updateChars = () => {
+  onRequest = (offset) => {
+    this.omCharListLoading();
     this.marvelService
-        .getAllCharacters()
-        .then(this.onCharLoaded)
-        .catch(this.onError);
-    };
+      .getAllCharacters(offset)
+      .then(this.onCharsLoaded)
+      .catch(this.onError);
+  };
 
-    render() {
-        return (
-        <div className="char__list">
-            <ul className="char__grid">
-                <li className="char__item">
+  omCharListLoading = () => {
+    this.setState({ newItemLoading: true });
+  };
+
+  onCharsLoaded = (newChars) => {
+    let ended = false;
+    if (newChars.length < 9) {
+      ended = true;
+    }
+    this.setState(({ chars, offset }) => ({
+      chars: [...chars, ...newChars],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
+  };
+
+  onError = () => {
+    this.setState({
+      loading: false,
+      error: true,
+    });
+  };
+
+  renderItems(chars) {
+    return chars.map((char) => {
+      let imgStyle = { objectFit: "cover" };
+      if (
+        char.thumbnail ===
+        "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+      ) {
+        imgStyle = { objectFit: "contain" };
+      }
+      return (
+        <li
+          className="char__item"
+          key={char.id}
+          onClick={() => this.props.onCharSelected(char.id)}
+        >
+          <img src={char.thumbnail} alt={char.name} style={imgStyle} />
+          <div className="char__name">{char.name}</div>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    const { chars, loading, error, newItemLoading, offset, charEnded } = this.state;
+
+    const items = this.renderItems(chars);
+
+    const errorMessage = error ? <ErrorMessage /> : null;
+    const spinner = loading ? <Spinner /> : null;
+    const content = !(loading || error) ? items : null;
+
+    return (
+      <div className="char__list">
+        <ul className="char__grid">
+          {errorMessage}
+          {spinner}
+          {content}
+          {/* <li className="char__item">
                     <img src={abyss} alt="abyss"/>
                     <div className="char__name">Abyss</div>
                 </li>
@@ -68,15 +126,19 @@ class CharList extends Component{
                 <li className="char__item">
                     <img src={abyss} alt="abyss"/>
                     <div className="char__name">Abyss</div>
-                </li>
-            </ul>
-            <button className="button button__main button__long">
-                <div className="inner">load more</div>
-            </button>
-        </div>
-    )
-    }
-    
+                </li> */}
+        </ul>
+        <button
+          className="button button__main button__long"
+          disabled={newItemLoading}
+          style={{'display': charEnded ? "none" : "block"}}
+          onClick={() => this.onRequest(offset)}
+        >
+          <div className="inner">load more</div>
+        </button>
+      </div>
+    );
+  }
 }
 
 export default CharList;
